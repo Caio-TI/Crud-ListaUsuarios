ecba2335d3bfdce4c19e02efc24d2e6a5e32e3d0
Caio Salomao <caio.gsalomao@outlook.com>
2020-03-22 12:04:19 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { CreateUserComponent } from './users/create-user/create-user.component';
+import { FormsModule } from "@angular/forms";
+import { UpdateUserComponent } from './users/update-user/update-user.component';
+import { DeleteUserComponent } from './users/delete-user/delete-user.component';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    CreateUserComponent,
+    UpdateUserComponent,
+    DeleteUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    // BrowserAnimationsModule,
+    // MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
